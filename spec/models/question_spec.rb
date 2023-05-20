a83aaad96b9a26c6f8a519c113973e6a66d44f16
redockirtap <redockirtap@gmail.com>
2023-05-20 15:37:01 +0300

    refactor question model validation, migration and tests

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-
-  context 'when question title is valid' do
-    it { is_expected.to validate_presence_of :title }
+  context 'Model association' do
+    it { is_expected.to have_many(:answers) }
   end
-  context 'when question body is valid' do
+
+  context 'Model validation' do
+    it { is_expected.to validate_presence_of :title }
     it { is_expected.to validate_presence_of :body }
   end
 end
