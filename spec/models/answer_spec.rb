d594ece6567d6f016c0dc162d7bcef1935b6aca2
redockirtap <redockirtap@gmail.com>
2023-05-20 15:38:09 +0300

    create answer model, association and validation; add tests

---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Answer, type: :model do
+  context 'Database table' do
+    it { is_expected.to have_db_index(:question_id) }
+  end
+
+  context 'Model association' do
+    it { is_expected.to belong_to(:question) }
+  end
+
+  context 'Model validation' do
+    it { is_expected.to validate_presence_of :body }
+  end
+end
