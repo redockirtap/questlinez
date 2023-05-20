12e11088cb9c1892dcf549d0e56e8ea368a308be
redockirtap <redockirtap@gmail.com>
2023-05-20 16:25:34 +0300

    add dependant on destroy attribute to question model, and its test

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Question, type: :model do
   context 'Model association' do
-    it { is_expected.to have_many(:answers) }
+    it { is_expected.to have_many(:answers).dependent(:destroy) }
   end
 
   context 'Model validation' do
