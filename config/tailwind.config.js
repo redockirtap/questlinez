a831a88cecbe8394604e71e02a7a73b170096535
redockirtap <redockirtap@gmail.com>
2023-06-03 18:02:00 +0300

    add tailwind css

---
+++ config/tailwind.config.js
@@ -0,0 +1,32 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'bg-main': '#2d2d2d',
+        'main-text': '#fafafa',
+        'border-main': '#474B4D',
+        'btn-primary': '#0964AA',
+      },
+      fontSize: {
+        main: '16px',
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+  ]
+}
